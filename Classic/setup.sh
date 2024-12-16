b12219f7f4991804187c58179be261e0b9b8e6ab
Tronco2018 <130449924+Tronco2018@users.noreply.github.com>
2024-12-16 08:45:21 +0100

    Modified setup.sh
    Added chmod +x on the main script

--- Classic/setup.sh
+++ Classic/setup.sh
@@ -3,5 +3,6 @@ mkdir ~/.config/troncofetch
 echo "Copying configs..."
 cp res/ascii.txt ~/.config/troncofetch
 echo "Copying main script...."
+sudo chmod +x bin/troncofetch.sh
 sudo cp bin/troncofetch.sh /usr/bin/troncofetch
 echo "DONE!"
